7f45b90a079196a594f8073a6318d4177d9f508e
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2022-02-12 01:21:31 -0500

    Added initial ZSH

---
+++ README.md
@@ -0,0 +1,10 @@
+# Environment
+
+Initial script to setup my virtual environment.
+
+## Scripts
+
+windows.ps1 - Sets up PowerShell environment.
+
+## Authors
+* [Daniel Ribeirinha-Braga](https://github.com/DBragz)
---
+++ unix.sh
@@ -0,0 +1,10 @@
+# Set up zsh tools
+PATH_TO_ZSH_DIR=$HOME/.oh-my-zsh
+if [ -d $PATH_TO_ZSH_DIR ]
+then
+   echo "\n$PATH_TO_ZSH_DIR directory exists! Skipping installation of zsh tools."
+else
+   echo "⤵ Configuring zsh tools in the $HOME directory..."
+   (cd $HOME && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended)
+   echo "✅ Successfully installed zsh tools"
+fi
