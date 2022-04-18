d62f34dc754f5ec07e69a59b6bbd6f781375dc43
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2022-04-17 21:03:11 -0400

    Added windows terminal.

---
+++ windows/windows-terminal.ps1
@@ -0,0 +1,11 @@
+<# .SYNOPSIS
+	Windows terminal install
+.DESCRIPTION
+	Script for installing windows terminal
+.NOTES
+	Author: Daniel Ribeirinha-Braga - dmrbraga@gmail.com
+.LINK
+	https://github.com/DBragz/environment/blob/main/windows/windows-terminal.ps1
+#>
+
+winget install Microsoft.WindowsTerminal
