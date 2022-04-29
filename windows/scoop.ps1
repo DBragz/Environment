684d496721c1946f5e51620df5ce5a0244e360e8
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2022-04-29 16:41:58 -0400

    Adding scoop program.

---
+++ windows/scoop.ps1
@@ -0,0 +1,11 @@
+<# .SYNOPSIS
+	Scoop terminal install
+.DESCRIPTION
+	Script for installing scoop.
+.NOTES
+	Author: Daniel Ribeirinha-Braga - dmrbraga@gmail.com
+.LINK
+	https://github.com/DBragz/environment/blob/main/windows/scoop.ps1
+#>
+
+iwr -useb get.scoop.sh | iex
