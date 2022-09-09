3130b68e670cfd4c7825fa5bf7b8aa8771e2aaef
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2022-09-09 01:44:27 -0400

    Forgot to save proxy script

--- windows/powershell/proxy.ps1
+++ windows/powershell/proxy.ps1
@@ -8,5 +8,9 @@
 	https://github.com/DBragz/environment/blob/main/windows/environment-proxy.ps1
 #>
 
+if ((-not $args[0])) {
+  return Write-Host "Error: Parameters missing`nUsage: proxy [<proxy>]"
+}
+
 [System.Environment]::SetEnvironmentVariable('HTTP_PROXY', $args[0], [System.EnvironmentVariableTarget]::User)
 [System.Environment]::SetEnvironmentVariable('HTTPS_PROXY', $args[0], [System.EnvironmentVariableTarget]::User)
