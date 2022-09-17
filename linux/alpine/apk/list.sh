8842512cde720c1f97becbaa022089cd183662f1
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2022-09-17 14:50:22 -0400

    Update list.sh

--- linux/alpine/apk/list.sh
+++ linux/alpine/apk/list.sh
@@ -1,2 +1,12 @@
+#!/bin/ash
+#
+# Script Name: list.sh
+#
+# Author: Daniel Ribeirinha-Braga
+#
+# Description: Script for viewing all Alpine Package Keeper (APK) packages.
+#
+
+
 chmod o+r /etc/apk/world
 cat /etc/APK/world
