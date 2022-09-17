272417dc37d5cf0228c1d1cd23271f592e3ccf8b
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2022-09-17 09:56:46 -0400

    Update powershell.sh

--- linux/alpine/apk/powershell.sh
+++ linux/alpine/apk/powershell.sh
@@ -1,5 +1,5 @@
 # install the requirements
-sudo apk add --no-cache \
+apk add --no-cache \
     ca-certificates \
     less \
     ncurses-terminfo-base \
@@ -14,23 +14,23 @@ sudo apk add --no-cache \
     icu-libs \
     curl
 
-sudo apk -X https://dl-cdn.alpinelinux.org/alpine/edge/main add --no-cache \
+apk -X https://dl-cdn.alpinelinux.org/alpine/edge/main add --no-cache \
     lttng-ust
 
 # Download the powershell '.tar.gz' archive
 curl -L https://github.com/PowerShell/PowerShell/releases/download/v7.2.6/powershell-7.2.6-linux-alpine-x64.tar.gz -o /tmp/powershell.tar.gz
 
 # Create the target folder where powershell will be placed
-sudo mkdir -p /opt/microsoft/powershell/7
+mkdir -p /opt/microsoft/powershell/7
 
 # Expand powershell to the target folder
-sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
+tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
 
 # Set execute permissions
-sudo chmod +x /opt/microsoft/powershell/7/pwsh
+chmod +x /opt/microsoft/powershell/7/pwsh
 
 # Create the symbolic link that points to pwsh
-sudo ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
+ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
 
 # Start PowerShell
 pwsh
