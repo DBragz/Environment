658bc34264f7805ceaaa7763d95967cdfb7cff1d
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2022-09-17 09:04:42 -0400

    Update node.js

--- linux/alpine/apk/node.js
+++ linux/alpine/apk/node.js
@@ -1,1 +1,8 @@
 
+echo "https://dl-cdn.alpinelinux.org/alpine/v3.12/main" >> /etc/apk/repositories
+echo "https://dl-cdn.alpinelinux.org/alpine/v3.12/community" >> /etc/apk/repositories
+
+apk update
+
+apk add nodejs=12.22.12-r0
+apk add npm=12.22.12-r0
