8e648ac9d1f109187b6d6c1fb8537f4db7d5d37f
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2022-09-18 00:20:56 -0400

    Updating vim plugin.

--- linux/alpine/curl/vim_plug.sh
+++ linux/alpine/curl/vim_plug.sh
@@ -7,5 +7,5 @@
 # Description: Script for installing VIM Plug.
 #
 
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
