8870eef4b255e7f245999b772d42401a13c47465
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2022-09-18 00:12:23 -0400

    Added vim plug.

---
+++ linux/alpine/curl/vim_plug.sh
@@ -0,0 +1,11 @@
+#!/bin/ash
+#
+# Script Name: vim_plug.sh
+#
+# Author: Daniel Ribeirinha-Braga
+#
+# Description: Script for installing VIM Plug.
+#
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
