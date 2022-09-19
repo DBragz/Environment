7658dd04979f92ade8cc6c52e8989ca0e197bc1a
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2022-09-19 19:03:56 -0400

    Added guard check for #9

--- mac/bash/brew.sh
+++ mac/bash/brew.sh
@@ -7,6 +7,12 @@
 # Description: Script for setting up Homebrew package manager.
 #
 
+if [ -z $1 ]
+then
+  echo -e "Error: Parameter missing\nUsage: brew.sh [<username>]"
+  exit 0
+fi
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo ‘eval “$(/opt/homebrew/bin/brew shellenv)”’ >> /Users/$1/.zprofile
