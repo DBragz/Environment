ad2831d5b6e7f2bf403aac3a2ffd2846862f276e
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2022-09-22 20:40:58 -0400

    Removed reboot and script.

--- windows/powershell/path.ps1
+++
@@ -1,9 +0,0 @@
-<# .SYNOPSIS
-	Set Path
-.DESCRIPTION
-	Script for adding a directory to the environment path.
-.NOTES
-	Author: Daniel Ribeirinha-Braga
-#>
-
-[Environment]::SetEnvironmentVariable("PATH", $Env:PATH + ";" + ($args[0]), [EnvironmentVariableTarget]::Machine)
