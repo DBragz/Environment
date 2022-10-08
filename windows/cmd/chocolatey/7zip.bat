4ea1e4fe7f5f7701f20c817955c0468a274651bd
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2022-10-08 19:26:10 -0400

    Added first name

--- windows/cmd/chocolatey/7zip.bat
+++ windows/cmd/chocolatey/7zip.bat
@@ -1,6 +1,6 @@
 @REM Name:     7zip.bat
 @REM Purpose:  Script for installing 7-Zip.
-@REM Author:   dmrbraga@gmail.com
+@REM Author:   Daniel Ribeirinha-Braga - dmrbraga@gmail.com
 
 @echo off
 
--- windows/cmd/chocolatey/docker.bat
+++ windows/cmd/chocolatey/docker.bat
@@ -1,6 +1,6 @@
 @REM Name:     docker.bat
 @REM Purpose:  Script for installing Docker.
-@REM Author:   dmrbraga@gmail.com
+@REM Author:   Daniel Ribeirinha-Braga - dmrbraga@gmail.com
 
 @echo off
 
