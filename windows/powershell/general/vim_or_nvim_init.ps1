a34ef96684f1e89502c5a0322a9f64cca6fc9368
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2022-10-15 21:38:45 -0400

    Forgot to remove commented out code.

--- windows/powershell/general/vim_or_nvim_init.ps1
+++ windows/powershell/general/vim_or_nvim_init.ps1
@@ -1,26 +1,3 @@
-# @REM Name:     vim_or_nvim_init.bat
-# @REM Purpose:  Script for initializing Vim Plugin Manager.
-# @REM Author:   Daniel Ribeirinha-Braga - dmrbraga@gmail.com
-
-# @echo off
-
-# mkdir %HOMEPATH%\vimfiles\autoload\
-
-# if [%1] == [] (
-#   echo Error: Parameters missing
-#   echo usage: vim_plug.bat [vim ^| nvim]
-#   goto :eof
-# ) else (
-#   if %1 == vim (
-#     copy configs/.vimrc %HOMEPATH%/.vimrc
-#     goto :eof
-#   )
-#   if %1 == nvim (
-#     copy configs/init.vim %HOMEPATH%/AppData/Local/nvim/init.vim
-#     goto :eof
-#   )
-# )
-
 <# .SYNOPSIS
 	Vim or Neovim Setup
 .DESCRIPTION
