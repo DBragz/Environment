2f7ec87c54f3bbaacfb6883daa742adb666b663e
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2022-10-28 18:49:57 -0400

    Added Nix installation.

---
+++ unix/mac/nix.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# Script Name: nix.sh
+#
+# Author: Daniel Ribeirinha-Braga - dmrbraga@gmail.com
+#
+# Description: Script for installing Nix.
+#
+
+sh <(curl -L https://nixos.org/nix/install)
