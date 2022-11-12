92da09752e1e9f747105dd05ccfa3c7b928fdd91
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2022-11-12 15:45:31 -0700

    Delete docker_proxy.sh

--- unix/docker/docker_proxy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-#
-# Script Name: docker_proxy.sh
-#
-# Author: Daniel Ribeirinha-Braga - dmrbraga@gmail.com
-#
-# Description: Script for setting up the the proxy for Docker containers.
-#
