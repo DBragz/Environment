e9235507974da4dbdc871e76943ae45faa0e27e3
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2024-09-30 21:16:09 -0400

    Cleaned up ADB install.

--- windows/powershell/chocolatey/adb.ps1
+++ windows/powershell/chocolatey/adb.ps1
@@ -1,10 +1,15 @@
-<# .SYNOPSIS
-	Android Debug Bridge (ADB) install
+<# 
+.SYNOPSIS
+  Android Debug Bridge (ADB) install.
+
 .DESCRIPTION
-	Script for installing Android Debug Bridge.
+  Script for installing Android Debug Bridge.
+
+.FILE
+  abd.ps1
+
 .NOTES
-  Name:   abd.ps1
-	Author: Daniel Ribeirinha-Braga
+  Author: Daniel Ribeirinha-Braga
 #>
 
 choco install adb -y
